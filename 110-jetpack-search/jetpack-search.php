4c4746efc948e69b98f4010c580df84a194de21b
Roger Theriault <roger@rogertheriault.com>
2019-01-10 15:53:05 -0500

    more jetpack search docs

--- 110-jetpack-search/jetpack-search.php
+++ 110-jetpack-search/jetpack-search.php
@@ -1,17 +1,17 @@
 /**
-1726	    * Enable Elastic search for category, tag and author pages.
-1727	    *
-1728	    * @param $query
-1729	    */
-1730	    public function maybe_enable_es_wp_query( $query ) {
-1731	
-1732	        if ( is_admin() || ! $query->is_main_query() ) {
-1733	            return;
-1734	        }
-1735	
-1736	        if ( $query->is_category() || $query->is_tag() || $query->is_author() ) {
-1737	            $query->set( 'es', 'true' );
-1738	        }
-1739	    }
+* Enable Elastic search for category, tag and author pages.
+*
+* @param $query
+*/
+public function my_maybe_enable_es_wp_query( $query ) {
 
-add_action( 'pre_get_posts', [ $this, 'maybe_enable_es_wp_query' ] );
+        if ( is_admin() || ! $query->is_main_query() ) {
+                return;
+        }
+
+        if ( $query->is_category() || $query->is_tag() || $query->is_author() ) {
+                $query->set( 'es', 'true' );
+        }
+}
+
+add_action( 'pre_get_posts', 'my_maybe_enable_es_wp_query' );
