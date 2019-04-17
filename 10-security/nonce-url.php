35cad759988af7c65fd6457cbbab865028d89b7f
David Sword <david@davidsword.ca>
2019-04-17 16:21:21 -0700

    improve inline comments & some naming

--- 10-security/nonce-url.php
+++ 10-security/nonce-url.php
@@ -5,17 +5,29 @@ add_action( 'admin_menu', function() {
 		'URL Nonce Example',
 		'URL Nonce Example',
 		'publish_posts',
-		'vip__url_link',
-		'vip__url_link_callback'
+		'vip__nonce_url_link',
+		'vip__nonce_url_link_callback'
 	);
 } );
 
 // Contents for the example menu page.
-function vip__url_link_callback () {
-	$url       = admin_url('options.php?page=my_plugin_settings');
-	$nonce_url = wp_nonce_url( $url, 'vip__nonce_action', 'vip__nonce_name' );
+function vip__nonce_url_link_callback () {
 
-	if ( ! isset( $_GET['vip__nonce_name'] ) || ! wp_verify_nonce( $_GET['vip__nonce_name'], 'vip__nonce_action' ) ) {
+	$url = admin_url('options.php?page=vip__nonce_url_link');
+
+	/**
+	 * Create a nonce URL.
+	 *
+	 * @see https://codex.wordpress.org/Function_Reference/wp_nonce_url
+	 */
+	$nonce_url = wp_nonce_url( $url, 'vip__nonce_action', '_vip__nonce' );
+
+	/**
+	 * Verify the nonce exists & verify it's correct.
+	 *
+	 * @see https://codex.wordpress.org/Function_Reference/wp_verify_nonce
+	 */
+	if ( ! isset( $_GET['_vip__nonce'] ) || ! wp_verify_nonce( $_GET['_vip__nonce'], 'vip__nonce_action' ) ) {
 		?>
 		<a href="<?php $nonce_url ?>" class="button button-primary">Nonce URL »</a>
 		<?php
