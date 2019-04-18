b6f095e7ee229e6821feb38a2b4f843eb4561265
David Sword <david@davidsword.ca>
2019-04-18 09:55:58 -0700

    change logic so eaiser to read
    the if/else before was sort of weird to read.
    
    this is clearer as the validation is before the creating of the nonce

--- 10-security/nonce-url.php
+++ 10-security/nonce-url.php
@@ -12,6 +12,17 @@ add_action( 'admin_menu', function() {
 
 // Contents for the example menu page.
 function vip__nonce_url_link_callback () {
+	/**
+	 * Verify the nonce exists & verify it's correct.
+	 *
+	 * @see https://codex.wordpress.org/Function_Reference/wp_verify_nonce
+	 */
+	if ( isset( $_GET['_vip__nonce'] ) && wp_verify_nonce( $_GET['_vip__nonce'], 'vip__nonce_action' ) ) {
+		?>
+		<p>✅ Valid nonce.</p>
+		<?php
+		return;
+	}
 
 	$url = admin_url('options.php?page=vip__nonce_url_link');
 
@@ -21,19 +32,7 @@ function vip__nonce_url_link_callback () {
 	 * @see https://codex.wordpress.org/Function_Reference/wp_nonce_url
 	 */
 	$nonce_url = wp_nonce_url( $url, 'vip__nonce_action', '_vip__nonce' );
-
-	/**
-	 * Verify the nonce exists & verify it's correct.
-	 *
-	 * @see https://codex.wordpress.org/Function_Reference/wp_verify_nonce
-	 */
-	if ( ! isset( $_GET['_vip__nonce'] ) || ! wp_verify_nonce( $_GET['_vip__nonce'], 'vip__nonce_action' ) ) {
-		?>
-		<a href="<?php $nonce_url ?>" class="button button-primary">Nonce URL »</a>
-		<?php
-	} else {
-		?>
-		<p>✅ Valid nonce, the link was clicked.</p>
-		<?php
-	}
+	?>
+	<a href="<?php echo esc_url( $nonce_url ); ?>" class="button button-primary">Nonce URL »</a>
+	<?php
 }
