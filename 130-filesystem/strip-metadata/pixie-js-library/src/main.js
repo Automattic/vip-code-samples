a0bf13d3670eae6846fad0c9dc46ea361fde9180
Ovidiu Liuta <ovidiu.liuta@automattic.com>
2021-12-27 09:06:37 +0200

    lint changes

--- 130-filesystem/strip-metadata/pixie-js-library/src/main.js
+++ 130-filesystem/strip-metadata/pixie-js-library/src/main.js
@@ -1,38 +1,38 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  const fileInput = document.getElementById("fileInput");
+	const fileInput = document.getElementById("fileInput");
 
-  fileInput.addEventListener("change", (e) => {
-    // get a reference to the file
-    const file = e.target.files[0];
+	fileInput.addEventListener("change", (e) => {
+		// get a reference to the file
+		const file = e.target.files[0];
 
-    if (!file.type.match("image/jpeg.*")) {
-      alert("Please select a JPEG image");
-      return;
-    }
+		if (!file.type.match("image/jpeg.*")) {
+			alert("Please select a JPEG image");
+			return;
+		}
 
-    // encode the file using the FileReader API
-    const reader = new FileReader();
-    reader.onloadend = (e) => {
-      // get the base64String from FileReader API
-      const base64String = reader.result;
+		// encode the file using the FileReader API
+		const reader = new FileReader();
+		reader.onloadend = (e) => {
+			// get the base64String from FileReader API
+			const base64String = reader.result;
 
-      // example to strip to strip all Exif metadata
-      //base64String = piexif.remove(e.target.result);
+			// example to strip to strip all Exif metadata
+			//base64String = piexif.remove(e.target.result);
 
-      const newExifData = piexif.load(base64String);
-      console.log("Original GPS Data:", newExifData.GPS);
+			const newExifData = piexif.load(base64String);
+			console.log("Original GPS Data:", newExifData.GPS);
 
-      // strip exif GPS data
-      const newPhotoData = stripExifData(base64String);
+			// strip exif GPS data
+			const newPhotoData = stripExifData(base64String);
 
-      // verify that new image has GPS data removed by loading the Exif data of the new image
-      verifyGPSExifData(newPhotoData);
+			// verify that new image has GPS data removed by loading the Exif data of the new image
+			verifyGPSExifData(newPhotoData);
 
-      // display the new image in the browser
-      displayImage(newPhotoData);
-    };
-    reader.readAsDataURL(file);
-  });
+			// display the new image in the browser
+			displayImage(newPhotoData);
+		};
+		reader.readAsDataURL(file);
+	});
 });
 
 /**
@@ -42,35 +42,34 @@ document.addEventListener("DOMContentLoaded", function (event) {
  * @returns base64String
  */
 const stripExifData = (base64String) => {
-  const newExif = piexif.load(base64String);
+	const newExif = piexif.load(base64String);
 
-  // remove GPS exif data
-  delete newExif.GPS;
+	// remove GPS exif data
+	delete newExif.GPS;
 
-  // Convert the new Exif object into binary form
-  const newExifBinary = piexif.dump(newExif);
+	// Convert the new Exif object into binary form
+	const newExifBinary = piexif.dump(newExif);
 
-  // Embed the Exif data into the image data and return the new image data
-  return piexif.insert(newExifBinary, base64String);
+	// Embed the Exif data into the image data and return the new image data
+	return piexif.insert(newExifBinary, base64String);
 };
 
 const verifyGPSExifData = (base64String) => {
-  // verify that new image has GPS data removed by loading the Exif data of the new image
-  const newExifData = piexif.load(base64String);
+	// verify that new image has GPS data removed by loading the Exif data of the new image
+	const newExifData = piexif.load(base64String);
 
-  if (
-    !newExifData.hasOwnProperty("GPS") ||
-    (newExifData.hasOwnProperty("GPS") &&
-      Object.keys(newExifData.GPS).length === 0)
-  ) {
-    console.log("GPS Data Removed");
-  } else {
-    console.log("GPS Data Found:", newExifData.GPS);
-  }
+	if (
+		!newExifData.hasOwnProperty("GPS") ||
+		(newExifData.hasOwnProperty("GPS") &&
+			Object.keys(newExifData.GPS).length === 0)
+	) {
+		console.log("GPS Data Removed");
+	} else {
+		console.log("GPS Data Found:", newExifData.GPS);
+	}
 };
 
 const displayImage = (imageData) => {
-  document.getElementById("base64image").src = imageData;
-  document.getElementById("base64image").style.display = "block";
+	document.getElementById("base64image").src = imageData;
+	document.getElementById("base64image").style.display = "block";
 };
-
