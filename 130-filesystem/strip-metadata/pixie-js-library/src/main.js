b5dcac92c0c97641f703ca96d47ba263e25fe079
Ovidiu Liuta <ovidiu.liuta@automattic.com>
2021-12-27 09:54:35 +0200

    prettier config changes

--- 130-filesystem/strip-metadata/pixie-js-library/src/main.js
+++ 130-filesystem/strip-metadata/pixie-js-library/src/main.js
@@ -1,43 +1,43 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-    const fileInput = document.getElementById('fileInput');
+	const fileInput = document.getElementById('fileInput');
 
-    fileInput.addEventListener('change', (e) => {
-        // get a reference to the file
-        const file = e.target.files[0];
+	fileInput.addEventListener('change', (e) => {
+		// get a reference to the file
+		const file = e.target.files[0];
 
-        if (!file.type.match('image/jpeg.*')) {
-            alert('Please select a JPEG image');
-            return;
-        }
+		if (!file.type.match('image/jpeg.*')) {
+			alert('Please select a JPEG image');
+			return;
+		}
 
-        // encode the file using the FileReader API
-        const reader = new FileReader();
-        reader.onloadend = (e) => {
-            // get the base64String from FileReader API
-            const base64String = reader.result;
-            let newPhotoData;
+		// encode the file using the FileReader API
+		const reader = new FileReader();
+		reader.onloadend = (e) => {
+			// get the base64String from FileReader API
+			const base64String = reader.result;
+			let newPhotoData;
 
-            const newExifData = piexif.load(base64String);
-            console.log('Original GPS Data:', newExifData.GPS);
+			const newExifData = piexif.load(base64String);
+			console.log('Original GPS Data:', newExifData.GPS);
 
-            if (document.getElementById('exif_remove_all').checked) {
-                console.log('Stripping all Exif data');
-                // example to strip to strip all Exif metadata
-                newPhotoData = piexif.remove(e.target.result);
-            } else {
-                console.log('Stripping only GPS Exif data');
-                // strip only Exif GPS metadata
-                newPhotoData = stripExifData(base64String);
-            }
+			if (document.getElementById('exif_remove_all').checked) {
+				console.log('Stripping all Exif data');
+				// example to strip to strip all Exif metadata
+				newPhotoData = piexif.remove(e.target.result);
+			} else {
+				console.log('Stripping only GPS Exif data');
+				// strip only Exif GPS metadata
+				newPhotoData = stripExifData(base64String);
+			}
 
-            // verify that new image has GPS data removed by loading the Exif data of the new image
-            verifyGPSExifData(newPhotoData);
+			// verify that new image has GPS data removed by loading the Exif data of the new image
+			verifyGPSExifData(newPhotoData);
 
-            // display the new image in the browser
-            displayImage(newPhotoData);
-        };
-        reader.readAsDataURL(file);
-    });
+			// display the new image in the browser
+			displayImage(newPhotoData);
+		};
+		reader.readAsDataURL(file);
+	});
 });
 
 /**
@@ -47,34 +47,34 @@ document.addEventListener('DOMContentLoaded', function (event) {
  * @returns base64String
  */
 const stripExifData = (base64String) => {
-    const newExif = piexif.load(base64String);
+	const newExif = piexif.load(base64String);
 
-    // remove GPS exif data
-    delete newExif.GPS;
+	// remove GPS exif data
+	delete newExif.GPS;
 
-    // Convert the new Exif object into binary form
-    const newExifBinary = piexif.dump(newExif);
+	// Convert the new Exif object into binary form
+	const newExifBinary = piexif.dump(newExif);
 
-    // Embed the Exif data into the image data and return the new image data
-    return piexif.insert(newExifBinary, base64String);
+	// Embed the Exif data into the image data and return the new image data
+	return piexif.insert(newExifBinary, base64String);
 };
 
 const verifyGPSExifData = (base64String) => {
-    // verify that new image has GPS data removed by loading the Exif data of the new image
-    const newExifData = piexif.load(base64String);
+	// verify that new image has GPS data removed by loading the Exif data of the new image
+	const newExifData = piexif.load(base64String);
 
-    if (
-        !newExifData.hasOwnProperty('GPS') ||
-        (newExifData.hasOwnProperty('GPS') &&
-            Object.keys(newExifData.GPS).length === 0)
-    ) {
-        console.log('GPS Data Removed');
-    } else {
-        console.log('GPS Data Found:', newExifData.GPS);
-    }
+	if (
+		!newExifData.hasOwnProperty('GPS') ||
+		(newExifData.hasOwnProperty('GPS') &&
+			Object.keys(newExifData.GPS).length === 0)
+	) {
+		console.log('GPS Data Removed');
+	} else {
+		console.log('GPS Data Found:', newExifData.GPS);
+	}
 };
 
 const displayImage = (imageData) => {
-    document.getElementById('base64image').src = imageData;
-    document.getElementById('base64image').style.display = 'block';
+	document.getElementById('base64image').src = imageData;
+	document.getElementById('base64image').style.display = 'block';
 };
