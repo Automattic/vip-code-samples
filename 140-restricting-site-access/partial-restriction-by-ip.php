03ab3700cdecf126dd85b96d79e78a034679f76d
Thiago Loureiro <thiago.loureiro@automattic.com>
2022-01-12 19:42:16 -0500

    checking if variable is defined

--- 140-restricting-site-access/partial-restriction-by-ip.php
+++ 140-restricting-site-access/partial-restriction-by-ip.php
@@ -4,6 +4,7 @@ define(
 	'VIP__ALLOWED_IPS',
 	array(
 		'172.25.0.2',
+		'172.18.0.1',
 		'192.168.32.1',
 	)
 );
@@ -24,7 +25,7 @@ add_action( 'wp_authenticate', 'vip__check_ips' );
 function vip__check_ips() {
 	if ( ! ( defined( 'WP_CLI' ) && WP_CLI ) ) {
      // phpcs:ignore WordPressVIPMinimum.Variables.ServerVariables.UserControlledHeaders, WordPress.Security.ValidatedSanitizedInput.InputNotValidated, WordPressVIPMinimum.Variables.RestrictedVariables.cache_constraints___SERVER__REMOTE_ADDR__
-		if ( ! in_array( $_SERVER['REMOTE_ADDR'], VIP__ALLOWED_IPS, true ) ) {
+		if ( defined( 'VIP__RESTRICTED_PATHS' ) && is_array( VIP__RESTRICTED_PATHS ) && ! in_array( $_SERVER['REMOTE_ADDR'], VIP__ALLOWED_IPS, true ) ) {
 			header( 'HTTP/1.0 403 Forbidden' );
 			exit;
 		}
@@ -42,7 +43,7 @@ function vip__check_restricted_pages() {
 
 	if ( ! ( defined( 'WP_CLI' ) && WP_CLI ) ) {
 
-	// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotValidated, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+		// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotValidated, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 		$current_path = $_SERVER['REQUEST_URI'];
 
 		$path_match = false;
