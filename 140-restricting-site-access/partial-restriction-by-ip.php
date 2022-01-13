7abe18baf20b7af977a7bad5de9d1e9611da34f2
Thiago Loureiro <thiago.loureiro@automattic.com>
2022-01-12 22:55:28 -0500

    checking the presence of constants correctly this time

--- 140-restricting-site-access/partial-restriction-by-ip.php
+++ 140-restricting-site-access/partial-restriction-by-ip.php
@@ -3,8 +3,8 @@
 define(
 	'VIP__ALLOWED_IPS',
 	array(
-		'172.25.0.2',
-		'172.18.0.1',
+		// '172.25.0.2',
+		// '172.18.0.1',
 		'192.168.32.1',
 	)
 );
@@ -25,7 +25,7 @@ add_action( 'wp_authenticate', 'vip__check_ips' );
 function vip__check_ips() {
 	if ( ! ( defined( 'WP_CLI' ) && WP_CLI ) ) {
      // phpcs:ignore WordPressVIPMinimum.Variables.ServerVariables.UserControlledHeaders, WordPress.Security.ValidatedSanitizedInput.InputNotValidated, WordPressVIPMinimum.Variables.RestrictedVariables.cache_constraints___SERVER__REMOTE_ADDR__
-		if ( defined( 'VIP__RESTRICTED_PATHS' ) && is_array( VIP__RESTRICTED_PATHS ) && ! in_array( $_SERVER['REMOTE_ADDR'], VIP__ALLOWED_IPS, true ) ) {
+		if ( defined( 'VIP__ALLOWED_IPS' ) && is_array( VIP__ALLOWED_IPS ) && ! in_array( $_SERVER['REMOTE_ADDR'], VIP__ALLOWED_IPS, true ) ) {
 			header( 'HTTP/1.0 403 Forbidden' );
 			exit;
 		}
@@ -48,7 +48,7 @@ function vip__check_restricted_pages() {
 
 		$path_match = false;
 
-		if ( defined( 'VIP__ALLOWED_IPS' ) && is_array( VIP__ALLOWED_IPS ) ) {
+		if ( defined( 'VIP__RESTRICTED_PATHS' ) && is_array( VIP__RESTRICTED_PATHS ) ) {
 			foreach ( VIP__RESTRICTED_PATHS as $restricted_path ) {
 				if ( $current_path === $restricted_path ) {
 					$path_match = true;
